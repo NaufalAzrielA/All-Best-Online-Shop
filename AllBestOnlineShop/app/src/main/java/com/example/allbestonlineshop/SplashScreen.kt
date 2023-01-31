0bc32239df52b247d03d131d663e3a00cf5f9ffe
NaufalAzrielA <123919442+NaufalAzrielA@users.noreply.github.com>
2023-01-31 10:46:00 +0700

    Add files via upload

---
+++ AllBestOnlineShop/app/src/main/java/com/example/allbestonlineshop/SplashScreen.kt
@@ -0,0 +1,11 @@
+package com.example.allbestonlineshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen_main)
+    }
+}
